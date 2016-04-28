78e07fccac4c92da5e25ebeec18980addf0e78ed
Jeroen Budts <jeroen@chromebook>
2016-04-28 21:29:45 +0200

    also change cycle layout back to control

--- rc.lua
+++ rc.lua
@@ -261,8 +261,8 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
     awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ "Control",           }, "space", function () awful.layout.inc(layouts,  1) end),
-    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
+    awful.key({ "Control",        }, "space", function () awful.layout.inc(layouts,  1) end),
+    awful.key({ "Control", "Shift"}, "space", function () awful.layout.inc(layouts, -1) end),
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
