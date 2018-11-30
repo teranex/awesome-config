fdb718888cc678eb6348f123acdfde691e422ffd
Jeroen Budts <jeroen@inuits.eu>
2018-11-30 09:37:29 +0100

    wallpapers: ignore files ending in _

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -3,5 +3,7 @@
 export DISPLAY=":0.0"
 
 wallpapers=~/Pictures/wallpapers
-image=`ls $wallpapers | sort -R | tail -n1`
-feh --bg-fill "$wallpapers/$image"
+# image=`ls $wallpapers | sort -R | tail -n1`
+image=`find $wallpapers -type f -not -name '*.*_' | sort -R | tail -n1`
+# feh --bg-fill "$wallpapers/$image"
+feh --bg-fill "$image"
