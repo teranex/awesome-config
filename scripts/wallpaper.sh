7a5b569355d3079c205eb31099c1c3f59403f94e
Jeroen Budts <jeroen@inuits.eu>
2020-01-10 21:04:59 +0100

    wallpaper: use differect dir and write current filename to tmp

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -2,8 +2,10 @@
 
 export DISPLAY=":0.0"
 
-wallpapers=~/Pictures/wallpapers
+wallpapers=/media/cryptdata/media/images/wallpapers
 # image=`ls $wallpapers | sort -R | tail -n1`
 image=`find $wallpapers -type f -not -name '*.*_' | sort -R | tail -n1`
 # feh --bg-fill "$wallpapers/$image"
 feh --bg-fill "$image"
+echo $image
+echo $image > /tmp/awesome-wallpaper.txt
